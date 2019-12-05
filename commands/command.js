6e7e22dcbbe39b436be6791ade47651f8dfb66d2
Daniel van Maren <blackv1ruz50@gmail.com>
2019-12-05 04:04:47 +0100

    Fixing capitalization

--- commands/command.js
+++ commands/command.js
@@ -1,8 +1,9 @@
 const Discord=require ("discord.js");
-const Config=("config-yml");
+const config=("config-yml");
 
 module.exports.run=async (bot, message, command, args) => {
     message.channel.send("Template command, not implemented");
+    
 }
 
 module.exports.meta={
@@ -10,7 +11,7 @@ module.exports.meta={
     name: "Nice Template",
     handles: ["template", "temp"],
     description: "This is a template command",
-    usage: `${Config.Prefix}template should be enough information`,
+    usage: `${config.prefix}template should be enough information`,
     permissions: [
         "template",
         "template.demo",
